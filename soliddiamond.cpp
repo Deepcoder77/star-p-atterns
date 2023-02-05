9b398956bd204486eb22a5ba4bd08dbb6cda70f8
Deepcoder77 <114980364+Deepcoder77@users.noreply.github.com>
2023-02-05 19:15:02 +0530

    Add files via upload

---
+++ soliddiamond.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+     int n;
+     cin>>n;
+     for(int i=0; i<n; i=i+1) {
+     for(int j=0;j<n-i-1;j++){
+        cout<<" ";
+     }
+     for(int k=0;k<i+1;k++){
+      cout<<"* ";
+     }
+     cout<<endl;
+     
+     }
+     for(int i=0; i<n; i=i+1) {
+       for(int j=0;j<i;j++){
+        cout<<" ";
+     }
+       for(int k=0;k<n-i;k++){
+      cout<<"* ";
+     }
+    
+    
+     cout<<endl;
+     }
+    return 0;
+}
